bb4f17c3a071115c36d0d9e11c77bdb19d947bd4
Frank Spee <frank.spee@bitfactory.nl>
2024-11-11 16:58:18 +0100

    refactor: :recycle: menu action into own file

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:test_flutter_mynotes/constants/routes.dart';
-
-enum MenuAction { logout }
+import 'package:test_flutter_mynotes/enums/menu_action.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
