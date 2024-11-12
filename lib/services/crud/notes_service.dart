f465b4481f186941e76bb15fd27ca1bdfc4b1116
Frank Spee <frank.spee@bitfactory.nl>
2024-11-12 15:00:16 +0100

    refactor: :recycle: move crud exceptions to own file

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -3,24 +3,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart'
     show MissingPlatformDirectoryException, getApplicationDocumentsDirectory;
 import 'package:path/path.dart' show join;
-
-class DatabaseAlreadyOpenException implements Exception {}
-
-class UnableToGetDocumentsDirectoryException implements Exception {}
-
-class DatabaseIsNotOpenException implements Exception {}
-
-class CouldNotDeleteUserException implements Exception {}
-
-class UserAlreadyExistsException implements Exception {}
-
-class CouldNotFindUserException implements Exception {}
-
-class CouldNotFindNoteException implements Exception {}
-
-class CouldNotDeleteNoteException implements Exception {}
-
-class CouldNotUpdateNoteException implements Exception {}
+import 'package:test_flutter_mynotes/services/crud/crud_exceptions.dart';
 
 class NotesService {
   Database? _db;
