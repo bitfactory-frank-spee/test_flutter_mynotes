f5feca13e7de3b136dd8acf4cb89328b748e2f3d
Frank Spee <frank.spee@bitfactory.nl>
2024-11-14 14:26:40 +0100

    refactor: :recycle: move notes view to notes folder

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:test_flutter_mynotes/constants/routes.dart';
 import 'package:test_flutter_mynotes/services/auth/auth_service.dart';
 import 'package:test_flutter_mynotes/views/login_view.dart';
-import 'package:test_flutter_mynotes/views/notes_view.dart';
+import 'package:test_flutter_mynotes/views/notes/notes_view.dart';
 import 'package:test_flutter_mynotes/views/register_view.dart';
 import 'package:test_flutter_mynotes/views/verify_email_view.dart';
 
