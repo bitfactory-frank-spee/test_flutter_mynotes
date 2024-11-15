0c856d5bf96b4e6906c3ae40ae93b546613bbda2
Frank Spee <frank.spee@bitfactory.nl>
2024-11-15 13:52:31 +0100

    feat: :lipstick: give create view update note padding

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -105,12 +105,15 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
 
             _setupTextControllerListener();
 
-            return TextField(
-              controller: _textController,
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              decoration: const InputDecoration(
-                hintText: 'Start typing your note...',
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                controller: _textController,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: const InputDecoration(
+                  hintText: 'Start typing your note...',
+                ),
               ),
             );
           }),
