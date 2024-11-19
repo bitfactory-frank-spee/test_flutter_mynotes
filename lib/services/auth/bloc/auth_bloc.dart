6b25b7b78cb56a656f3850ada58e139a064e2294
Frank Spee <frank.spee@bitfactory.nl>
2024-11-19 16:56:11 +0100

    refactor: :recycle: improve bloc auth states

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -9,7 +9,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       await provider.initialize();
       final user = provider.currentUser;
       if (user == null) {
-        emit(const AuthStateLoggedOut());
+        emit(const AuthStateLoggedOut(null));
       } else if (!user.isEmailVerified) {
         emit(const AuthStateNeedsVerification());
       } else {
@@ -17,7 +17,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       }
     });
     on<AuthEventLogIn>((event, emit) async {
-      emit(const AuthStateLoading());
       final email = event.email;
       final password = event.password;
       try {
@@ -27,14 +26,14 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         );
         emit(AuthStateLoggedIn(user));
       } on Exception catch (exception) {
-        emit(AuthStateLoginFailure(exception));
+        emit(AuthStateLoggedOut(exception));
       }
     });
     on<AuthEventLogOut>((event, emit) async {
-      emit(const AuthStateLoading());
       try {
+        emit(const AuthStateLoading());
         await provider.logOut();
-        emit(const AuthStateLoggedOut());
+        emit(const AuthStateLoggedOut(null));
       } on Exception catch (exception) {
         emit(AuthStateLogoutFailure(exception));
       }
